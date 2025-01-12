48b10206c92519ad4ce83408facd8d8921165558
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-12 16:54:11 +0530

    commit

---
+++ javascript/operation.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+let power = a ** b;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+console.log("Power:", power);
