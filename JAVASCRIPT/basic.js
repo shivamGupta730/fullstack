e7757b2dac61ae036e305676ea1995204b9a518f
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-15 12:05:43 +0530

    commit

---
+++ JAVASCRIPT/basic.js
@@ -0,0 +1,9 @@
+// alert("hello")
+let a=prompt("enter the first number");
+let b= prompt("enter the second number");
+if (a==b) {
+    alert("both are equal");
+}
+else{
+    alert("both are not equal");
+}
