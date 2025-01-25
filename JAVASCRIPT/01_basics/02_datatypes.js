7c7f5c8b8207a730008a7c1fb78751dd4351fe16
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-25 21:06:39 +0530

    datatypes

---
+++ JAVASCRIPT/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// treat all JS code as newer version
+
+// code readability should be high
+
+// number range is 2 to power 53
+// bigint
+// string==>""
+// boolean=>true/false
+// null=>standalone value
+// undefined
+// symbol==>unique value
+// console.log(typeof null); output => object
+// console.log(typeof undefined); output => undefined
+// object==>{} or new Object()
