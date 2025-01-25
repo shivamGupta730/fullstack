c71ea00b36d1b6e5cf8926d6065bde606c6afd9b
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-25 20:55:25 +0530

    commit

---
+++ JAVASCRIPT/01_basics/01_variables.js
@@ -0,0 +1,21 @@
+// Variables and constant
+const accountId=14453;
+let accountEmail="abc@gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+
+// "constant that cannot change"
+
+// accountId=2  not allowed
+// console.log(accountId);
+accountEmail="xyz@gmail.com";
+accountPassword="2123453";
+accountCity="mumbai";
+// console.table([accountEmail,accountId,accountPassword,accountCity])
+
+// var is a function scope variable
+// let is block scope variable
+/*
+prefer not to use var
+because of the issue in block scope and function scope
+*/ 
