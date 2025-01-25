ca55b8f5888d0bcbc972bac35c4d4f180de865d8
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-25 16:03:22 +0530

    change

--- JAVASCRIPT/basic.js
+++
@@ -1,9 +0,0 @@
-// alert("hello")
-let a=prompt("enter the first number");
-let b= prompt("enter the second number");
-if (a==b) {
-    alert("both are equal");
-}
-else{
-    alert("both are not equal");
-}
