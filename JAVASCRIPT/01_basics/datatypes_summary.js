320a777c2cf67f4e8973ffd91031a8bc0ea13a98
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-01-26 17:09:22 +0530

    datatypes summary

---
+++ JAVASCRIPT/01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+//  Primitive 
+
+// 7 types: number, string, boolean, null, undefined, symbol, bigint
+
+// javascript is dynamically typed, so you don't need to declare the type of a variable before using it.
+
+// reference(Non primitive)
+// object, array, function
+
+const heroes=["A","B"]
+
+let myObj={
+    name:"John",
+    age:30
+
+}
+
+const myFunction=function(){
+    console.log("Hello")
+}
+// to find type of datatypes
+console.log(typeof heroes); // object
